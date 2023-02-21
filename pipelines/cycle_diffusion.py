b22de996c187cb5197e484244864e4b2e482bb53
Jason-Choi <jasonchoi3@g.skku.edu>
2023-02-21 08:56:19 +0000

    [ADD] deepspeed acceleration

--- pipelines/cycle_diffusion.py
+++ pipelines/cycle_diffusion.py
@@ -3,13 +3,19 @@ from diffusers import CycleDiffusionPipeline, DDIMScheduler
 from config import DEVICE_ID, NUM_IMAGES_PER_PROMPT, NUM_INFERENCE_STEPS, DIFFUSION_PATH
 from PIL.Image import Image
 from typing import List
-
+import deepspeed
 
 scheduler = DDIMScheduler.from_pretrained(DIFFUSION_PATH, subfolder="scheduler")
 pipe = CycleDiffusionPipeline.from_pretrained(
     DIFFUSION_PATH, torch_dtype=torch.float16, scheduler=scheduler
 )
-pipe.to(f"cuda:{DEVICE_ID}")
+deepspeed.init_inference(
+    model=getattr(pipe,"model", pipe),      # Transformers models
+    mp_size=1,        # Number of GPU
+    dtype=torch.float16, # dtype of the weights (fp16)
+    replace_method="auto", # Lets DS autmatically identify the layer to replace
+    replace_with_kernel_inject=True, # replace the model with the kernel injector
+)
 
 
 
--- pipelines/stable_diffusion.py
+++ pipelines/stable_diffusion.py
@@ -3,12 +3,18 @@ from diffusers import StableDiffusionPipeline
 from config import DEVICE_ID, NUM_IMAGES_PER_PROMPT, NUM_INFERENCE_STEPS, DIFFUSION_PATH
 from PIL.Image import Image
 from typing import List
-
+import deepspeed
 
 pipe = StableDiffusionPipeline.from_pretrained(
     DIFFUSION_PATH, torch_dtype=torch.float16
 )
-pipe.to(f"cuda:{DEVICE_ID}")
+deepspeed.init_inference(
+    model=getattr(pipe,"model", pipe),      # Transformers models
+    mp_size=1,        # Number of GPU
+    dtype=torch.float16, # dtype of the weights (fp16)
+    replace_method="auto", # Lets DS autmatically identify the layer to replace
+    replace_with_kernel_inject=True, # replace the model with the kernel injector
+)
 
 
 
